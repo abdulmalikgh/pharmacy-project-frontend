7070247a2d74a9b8b42c4e5b66380d70df4c9484
abdulmalikgh <musah.abdulmalikgh@gmail.com>
2024-06-03 18:03:58 +0000

    add tenant branch store

---
+++ src/stores/tenantBranches.ts
@@ -0,0 +1,65 @@
+import { computed, ref, watch } from "vue";
+
+import ApiService from "../services/ApiService";
+import JwtService from "../services/JwtService";
+import { defineStore } from "pinia";
+import { useToast } from "vuestic-ui";
+
+
+export interface IAddStaf {
+    name:string;
+    location:string;
+    address:string;
+}
+
+export const useAuthStore = defineStore("tenantBranches", () => {
+    const branch = ref<IAddStaf>();
+    const branches = ref<IAddStaf[]>([]);
+    const loading = ref<Boolean>(false);
+
+    async function addBranch(payload: IAddStaf) {
+        loading.value = true;
+        try {
+            const { data } = await ApiService.post("/tenant/admin/branch", payload);
+            branch.value = data;
+            branches.value?.unshift(data)
+            return data;
+        } catch (error: any) {
+            throw error.response;
+        } finally {
+            loading.value = false;
+        };
+    };
+
+    async function fetchBranches() {
+        loading.value = true;
+        try {
+            const { data } = await ApiService.get("/tenant/admin/branch");
+            branches.value = data;
+            return data;
+        } catch (error: any) {
+            throw error.response;
+        } finally {
+            loading.value = false;
+        };
+    };
+
+    async function updateBranch(payload: any, index:any) {
+        loading.value = true;
+        try {
+            const { data } = await ApiService.put(`/tenant/admin/branch/${payload.id}`, payload);
+            branches.value[index] = data
+            return data;
+        } catch (error: any) {
+            throw error.response;
+        } finally {
+            loading.value = false;
+        };
+    };
+
+    return {
+        addBranch,
+        updateBranch,
+        fetchBranches,
+    };
+});
